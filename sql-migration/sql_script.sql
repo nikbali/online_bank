d2b6a9faa0a5b4ed2313dcfaaae2dc72107bda6e
nbalily <nikbali@mail.ru>
2018-08-02 22:39:47 +0300

    Добавил в SQL таблицу с ролями и inter таблицу между User и Role, и добавил два поля в таблицу User

--- sql-migration/sql_script.sql
+++ sql-migration/sql_script.sql
@@ -1,19 +1,25 @@
 CREATE DATABASE  IF NOT EXISTS `onlinebank`
 USE `onlinebank`;
 
+-- порядок важен
 DROP TABLE IF EXISTS `transaction`;
 DROP TABLE IF EXISTS `account`;
+DROP TABLE IF EXISTS `user_role`;
 DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `role`;
 
 CREATE TABLE `user` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `email` varchar(255) NOT NULL,
+  `document_number` int(10) NOT NULL,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
   `phone` varchar(255) DEFAULT NULL,
   `login` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_DOCUMENT` (`document_number`),
   UNIQUE KEY `UK_EMAIL` (`email`),
   UNIQUE KEY `UK_LOGIN` (`login`)
   ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -44,3 +50,25 @@ CREATE TABLE `transaction` (
   CONSTRAINT `FK_Sender` FOREIGN KEY (`sender_account_id`) REFERENCES `account`(`id`),
   CONSTRAINT `FK_Reciever` FOREIGN KEY (`receiver_account_id`) REFERENCES `account`(`id`)
  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ CREATE TABLE `role` (
+   `id` int(10) NOT NULL AUTO_INCREMENT,
+   `name` varchar(255) DEFAULT 'CLIENT',
+   PRIMARY KEY (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ -- добавим по умолчанию две роли
+ INSERT INTO `role` VALUES ('1','CLIENT'),('2','ADMIN');
+commit;
+
+ CREATE TABLE `user_role` (
+   `id` int(20) NOT NULL AUTO_INCREMENT,
+   `role_id` int(10) DEFAULT NULL,
+   `user_id` int(10) DEFAULT NULL,
+   PRIMARY KEY (`id`),
+   KEY `FKa68196081fvovjhkek5m97n3y` (`role_id`),
+   KEY `FK859n2jvi8ivhui0rl0esws6o` (`user_id`),
+   CONSTRAINT `FK859n2jvi8ivhui0rl0esws6o` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+   CONSTRAINT `FKa68196081fvovjhkek5m97n3y` FOREIGN KEY (`role_id`) REFERENCES `role` (`id`)
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
