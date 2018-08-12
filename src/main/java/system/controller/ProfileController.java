b2e164a11dfbfefb689036635f1d237a15fcb538
nbalily <nikbali@mail.ru>
2018-08-12 17:29:00 +0300

    На view cо счетами сделал обработчик кнопки по созданию нового Счета для клиента(макс. можно создать 5 счетов)

--- src/main/java/system/controller/ProfileController.java
+++ src/main/java/system/controller/ProfileController.java
@@ -5,17 +5,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import system.entity.Account;
 import system.entity.User;
+import system.service.AccountService;
 import system.service.UserService;
 import system.utils.UserUtils;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/main")
 public class ProfileController {
+
     @Autowired
     private UserService userService;
+    @Autowired
+    private AccountService accountService;
 
     @RequestMapping( method = RequestMethod.GET)
     public String profile(HttpSession session, Model model)
@@ -31,6 +37,23 @@ public class ProfileController {
         UserUtils.deleteUserFromSession(session);
         return "redirect:/";
 
+    }
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public String createNewAccountForClient(HttpSession session, Model model, HttpServletResponse res) {
+        User user = UserUtils.getUserFromSession(session);
+        if(user.getAccountList().size() >= 5)
+        {
+            res.setStatus(501);
+            res.addHeader("Error", "The customer can not create more than 5 accounts");
+            return null;
+        }
+        else {
+            Account account = accountService.createAccount(user);
+            user.getAccountList().add(account);
+            model.addAttribute("acc", account);
+            res.setStatus(201);
+            return "fragments :: account";
+        }
     }
 
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
