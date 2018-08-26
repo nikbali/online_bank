b7c00397c7e50728e2f0470e388e4211b6788c6e
VasilyevSergey <vasilyev.s@rambler.ru>
2018-08-26 17:38:50 +0300

    Сделал перевод средств между клиентами. Между аккаунтами одного клиента тоже переводит

--- src/main/java/system/controller/TransferController.java
+++ src/main/java/system/controller/TransferController.java
@@ -8,10 +8,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import system.entity.Account;
 import system.entity.Transaction;
 import system.entity.User;
+import system.service.AccountService;
 import system.service.TransactionService;
 import system.utils.UserUtils;
 
@@ -23,6 +25,9 @@ public class TransferController {
     @Autowired
     private TransactionService transactionService;
 
+    @Autowired
+    private AccountService accountService;
+
     private static final Logger log = LoggerFactory.getLogger(system.ApplicationWar.class);
 
     @RequestMapping(value = "", method = RequestMethod.GET)
@@ -33,7 +38,7 @@ public class TransferController {
         return "transfer";
     }
 
-    @RequestMapping(value="/toMyAccount", method=RequestMethod.GET)
+    @RequestMapping(value= "/toMyAccount", method=RequestMethod.GET)
     public String openTransferFormTest(HttpSession session, Model model){
         User user = UserUtils.getUserFromSession(session);
         String senderAccountNumber = "";
@@ -50,7 +55,6 @@ public class TransferController {
         model.addAttribute("amount", "0");
         model.addAttribute("user", user);
         model.addAttribute("all", user.getAccountList());
-
         return "toMyAccount";
     }
 
@@ -86,12 +90,48 @@ public class TransferController {
         return new ModelAndView("redirect:/main/transfer/toMyAccount");
     }
 
-    @RequestMapping(value = "/toClient", method = RequestMethod.GET)
-    public String toClient(HttpSession session, Model model)
-    {
+    @RequestMapping(value= "/toClient", method=RequestMethod.GET)
+    public String openTransferFormTest1(HttpSession session, Model model){
         User user = UserUtils.getUserFromSession(session);
+        String senderAccountNumber = "";
+        String receiverAccountNumber = "";
         model.addAttribute("user", user);
+        model.addAttribute("senderAccountNumber", senderAccountNumber);
+        model.addAttribute("receiverAccountNumber", receiverAccountNumber);
+
+        if(senderAccountNumber != null && !senderAccountNumber.equals("") && receiverAccountNumber != null && !receiverAccountNumber.equals("")){
+            model.addAttribute("accountsExist", true);
+        }else{
+            model.addAttribute("accountsExist", false);
+        }
+        model.addAttribute("amount", "0");
+        model.addAttribute("user", user);
+        model.addAttribute("all", user.getAccountList());
         return "toClient";
+    }
+
+    @RequestMapping(value="/toClient", method=RequestMethod.POST)
+    public ModelAndView sendTransferForm1(@ModelAttribute("amount") String amount, @ModelAttribute("senderAccountNumber") String senderAccountNumber,
+                                         @ModelAttribute("receiverAccountNumber") String receiverAccountNumber, HttpSession session)
+    {
+        log.info("Параметры amount: " + amount + " senderAccountNumber: " + senderAccountNumber + " receiverAccountNumber: " + receiverAccountNumber);
+        User user  = UserUtils.getUserFromSession(session);
+        Account senderAccount = null;
+        for (Account acc : user.getAccountList())
+        {
+            if(acc.getAccountNumber() == Long.parseLong(senderAccountNumber))
+            {
+                senderAccount = acc;
+                break;
+            }
+        }
+
+        Account receiverAccount = accountService.findByAccountNumber(Long.parseLong(receiverAccountNumber));
+
+        log.info("Перервод: " + amount + " RUB со счета: " + senderAccountNumber + " на счет: " + receiverAccountNumber);
+        Transaction transaction = transactionService.transfer(senderAccount, receiverAccount, Double.parseDouble(amount));
+        if(transaction != null) return new ModelAndView("redirect:/main/accounts");
+        return new ModelAndView("redirect:/main/transfer/toClient");
     }
 
     @RequestMapping(value = "/toAnotherBank", method = RequestMethod.GET)
