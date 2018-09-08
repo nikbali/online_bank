a60c6787003716b1702d17623c7fafbe09ccee56
nibali <nikbali@mail.ru>
2018-09-09 02:14:36 +0300

    Доработал Rest Controller. Методы для перевода, и проверки существования счета

--- src/main/java/system/rest_controller/Response.java
+++ src/main/java/system/rest_controller/Response.java
@@ -1,19 +1,51 @@
 package system.rest_controller;
 
+import org.springframework.http.HttpStatus;
+
+import java.time.Instant;
+
 public class Response {
-    private int errorCode;
-    private String errorMessage;
+    private int status;
+    private String error;
+    private String timestamp;
+    private String message;
 
-    public Response(int errorCode ,String errorMessage){
-        this.errorCode = errorCode;
-        this.errorMessage = errorMessage;
+    public Response(HttpStatus httpStatus,String errorMessage){
+        this.status = httpStatus.value();
+        this.error = httpStatus.getReasonPhrase();
+        this.message = errorMessage;
+        this.timestamp = Instant.now().toString();
     }
 
-    public String getErrorMessage() {
-        return errorMessage;
+    public int getStatus() {
+        return status;
     }
 
-    public int getErrorCode() {
-        return errorCode;
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
--- src/main/java/system/rest_controller/RestApiController.java
+++ src/main/java/system/rest_controller/RestApiController.java
@@ -1,5 +1,7 @@
 package system.rest_controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,56 +25,92 @@ public class RestApiController {
     @Autowired
     TransactionService transactionService;
 
-    @RequestMapping(value = "/transfer/", method = RequestMethod.POST)
-    public ResponseEntity<Response> transferFromOtherBank(@RequestBody Operation operation, @RequestHeader(value="x-api-key") String api_key) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(system.ApplicationWar.class);
 
+    /**
+     * Метод обрабатывает Post запрос для передачи средств со счета с внешнего банка на счет в наш банк
+     * @param operation Тело запроса содержит информацию о переводе
+     * @param api_key Уникальный ключ передается в header параметре x-api-key
+     * @return ответ в виде Json объекта, с результатом операции
+     */
+    @RequestMapping(value = "/transfer/", method = RequestMethod.POST)
+    public ResponseEntity<?> transferFromOtherBank(@RequestBody Operation operation, @RequestHeader(value="x-api-key") String api_key) {
+
+        LOGGER.info("Received GET request for transfer funds from another bank to {}", operation.getToAccount());
         ResponseEntity responseEntity = null;
         //здесь будем проверять api_key, пока просто от балды написал
-        if(api_key.equals("qwerty"))
-        {
-            if(validateInputParams(operation)){
-
-                Currency currency = Currency.valueOf(operation.getCurrency());
-                Account receiverAccount = accountService.findByAccountNumber(Long.parseLong(operation.getToAccount()));
-                Account senderAccount = accountService.findByAccountNumber(Long.parseLong(operation.getFromAccount()));
-
-                if (receiverAccount != null)
-                {
-                    //создаем аккаунт, если раньше такого не было
-                    if(senderAccount == null){
-                        senderAccount = new Account();
-                        senderAccount.setCurrency(currency);
-                        senderAccount.setBic(Integer.parseInt(operation.getBic()));
-                        senderAccount.setAccountNumber(Long.parseLong(operation.getFromAccount()));
-                    }
-                    if(currency == receiverAccount.getCurrency())
-                    {
-                        if (Bank.existBankByBic(Integer.parseInt(operation.getBic())))
-                        {
-                            Transaction transaction = transactionService.transferFromOtherBank(senderAccount, receiverAccount, BigDecimal.valueOf(operation.getAmount()), operation.getComment());
-                            if (transaction != null) {
-                                responseEntity = new ResponseEntity(new Response(0, "Success!"), HttpStatus.OK);
-                            } else
-                                responseEntity = new ResponseEntity(new Response(3, "Error Server transaction!"), HttpStatus.EXPECTATION_FAILED);
-                        }
-                        else responseEntity = new ResponseEntity(new Response(6, "Error! Currency is incorrectly specified !"), HttpStatus.EXPECTATION_FAILED);
-                    }
-                    else responseEntity = new ResponseEntity(new Response(5, "Error! Currency is incorrectly specified !"), HttpStatus.EXPECTATION_FAILED);
-                }
-                else
-                {
-                    responseEntity =new ResponseEntity(new Response(2,"Error! That Account Not Exist in our Bank."), HttpStatus.EXPECTATION_FAILED);
-                }
-            }
-            else responseEntity = new ResponseEntity(new Response(4,"Error! Incorrect request params!"), HttpStatus.EXPECTATION_FAILED);
+        if(!api_key.equals("qwerty")) {
+            LOGGER.error("Error API_KEY!");
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body(new Response(HttpStatus.FORBIDDEN,"Error API_KEY"));
         }
-        else
-        {
-            responseEntity = new ResponseEntity(new Response(1,"Error API_KEY"), HttpStatus.EXPECTATION_FAILED);
+        if(!validateInputParams(operation)) {
+            LOGGER.error("Error! Incorrect request params!");
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body(new Response(HttpStatus.NOT_ACCEPTABLE, "Error! Incorrect request params!"));
+
         }
-        return responseEntity;
+        Currency currency = Currency.valueOf(operation.getCurrency());
+        Account receiverAccount = accountService.findByAccountNumber(Long.parseLong(operation.getToAccount()));
+        Account senderAccount = accountService.findByAccountNumber(Long.parseLong(operation.getFromAccount()));
+        if (receiverAccount == null) {
+            LOGGER.error("Error! That Account Not Exist in our Bank.");
+            return   ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body(new Response(HttpStatus.NOT_ACCEPTABLE, "Error! That Account Not Exist in our Bank."));
+        }
+        //создаем аккаунт, если раньше такого не было
+        if(senderAccount == null){
+            senderAccount = new Account();
+            senderAccount.setCurrency(currency);
+            senderAccount.setBic(Integer.parseInt(operation.getBic()));
+            senderAccount.setAccountNumber(Long.parseLong(operation.getFromAccount()));
+        }
+        if(currency != receiverAccount.getCurrency()) {
+            LOGGER.error("Error! Currency is incorrectly specified for transfer to {}", operation.getToAccount());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body(new Response(HttpStatus.NOT_ACCEPTABLE, "Error! Currency is incorrectly specified!"));
+        }
+
+        if (!Bank.existBankByBic(Integer.parseInt(operation.getBic()))) {
+            LOGGER.error("Error! Unknown BIC for transfer to {}", operation.getToAccount());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body(new Response(HttpStatus.NOT_ACCEPTABLE, "Error! BIC is not acceptable!"));
+        }
+        Transaction transaction = transactionService.transferFromOtherBank(senderAccount, receiverAccount, BigDecimal.valueOf(operation.getAmount()), operation.getComment());
+        if (transaction == null) {
+            LOGGER.error("Error Server transaction for transfer to {}", operation.getToAccount());
+            return ResponseEntity
+                    .status(HttpStatus.NOT_IMPLEMENTED)
+                    .body(new Response(HttpStatus.NOT_IMPLEMENTED, "Error Server transaction!"));
+        }
+
+        LOGGER.info("Success transfer to {}", operation.getToAccount());
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(new Response(HttpStatus.OK, "Success!"));
     }
 
+    /**
+     * Метод обрабатывает Get запрос для проверки, существует ли счет в банке с указынным номером
+     * @param account_number
+     * @return ответ в виде Json объекта, с результатом
+     */
+    @RequestMapping(value = "/account/{account_number}", method = RequestMethod.GET)
+    public ResponseEntity<?> getUser(@PathVariable("account_number") long account_number) {
+        LOGGER.info("Fetching Account with account_number {}", account_number);
+        Account account = accountService.findByAccountNumber(account_number);
+        if (account == null) {
+            LOGGER.error("Account with account_number: {} not found.", account_number);
+            return new ResponseEntity(new Response(HttpStatus.NOT_FOUND,"Account with account_number: " + account_number
+                    + " not found"), HttpStatus.NOT_FOUND);
+        }
+        return  new ResponseEntity(new Response(HttpStatus.OK, "Success! Account with account_number: " + account_number + " exist."), HttpStatus.OK);
+    }
 
     public  static boolean validateInputParams(Operation operation) {
         if (operation.getFromAccount() != null &&
